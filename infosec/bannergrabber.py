14c414dbbfe9b2a66e4caec39f9a5d1773f1ff58
Turner Bell <turner@tbell.us>
2020-12-16 15:54:37 -0500

    add portscanner

--- infosec/bannergrabber.py
+++ infosec/bannergrabber.py
@@ -2,11 +2,14 @@
 
 import socket
 
-s = socket.socket()
+def banner(ip, port):
+    s = socket.socket()
+    s.connect((ip, int(port)))
+    print(str(s.recv(1024)).strip('b'))
 
-ip = input("Please enter the IP: ")
-port = str(input("Please enter the port: "))
+def main():
+    ip = input("Please enter the IP: ")
+    port = str(input("Please enter the port: "))
+    banner(ip, port)
 
-s.connect((ip, int(port)))
-
-print(s.recv(1024))
+main()
